e9c535549b0685d4e8ceeb0929017e55408ccb8a
GAUDIO DESIREE <136097544+desireee1234@users.noreply.github.com>
2026-02-17 22:45:35 +0100

    aggiunto cambio musica nelle prove

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixerSnapshot defaultSnapshot;
+    public AudioMixerSnapshot trialSnapshot;
+    public float transitionTime = 1f;
+
+    private void OnEnable()
+    {
+        SpellEvents.OnTrialStarted += HandleTrialStarted;
+        SpellEvents.OnTrialCompleted += HandleTrialEnded;
+        SpellEvents.OnTrialSuspended += HandleTrialEnded;
+    }
+
+    private void OnDisable()
+    {
+        SpellEvents.OnTrialStarted -= HandleTrialStarted;
+        SpellEvents.OnTrialCompleted -= HandleTrialEnded;
+        SpellEvents.OnTrialSuspended -= HandleTrialEnded;
+    }
+
+    void Start()
+    {
+        defaultSnapshot.TransitionTo(0f); // parte con musica base
+    }
+
+    private void HandleTrialStarted(int trialIndex)
+    {
+        trialSnapshot.TransitionTo(transitionTime);
+    }
+
+    private void HandleTrialEnded()
+    {
+        defaultSnapshot.TransitionTo(transitionTime);
+    }
+}
